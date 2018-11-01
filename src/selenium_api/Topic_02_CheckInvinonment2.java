dda33d783173ab17be56d33784a2465ab7a61eb2
DanTrang <DanTrang@DanTrang-PC>
2018-11-01 12:38:16 -0400

    Toppic 02

---
+++ src/selenium_api/Topic_02_CheckInvinonment2.java
@@ -0,0 +1,38 @@
+package selenium_api;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_02_CheckInvinonment2 {
+	WebDriver driver;
+
+	@BeforeClass
+	public void beforeClass() {
+		// Chrome
+		System.setProperty("webdriver.chrome.driver", ".\\lib\\chromedriver.exe");
+		driver = new ChromeDriver();
+
+		// Firefox
+		// driver = new FirefoxDriver();
+		driver.get("https://google.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void TC_01_CheckInvinonment() {
+		String homePageTittle = driver.getTitle();
+		Assert.assertEquals(homePageTittle, "Google");
+	}
+
+	@AfterClass
+	public void afterClass() {
+	}
+
+}

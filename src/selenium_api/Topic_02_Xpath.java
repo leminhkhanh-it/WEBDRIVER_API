942503ad936f3a36547a09accc176ff3d743082c
DanTrang <DanTrang@192.168.1.7>
2018-11-04 12:21:48 -0500

    Finish: Topic_02 TC_01_Verify_URL_and_title()

--- src/selenium_api/Topic_02_Xpath.java
+++ src/selenium_api/Topic_02_Xpath.java
@@ -2,8 +2,9 @@ package selenium_api;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -15,24 +16,31 @@ public class Topic_02_Xpath {
 	@BeforeClass
 	public void beforeClass() {
 		// Chrome
-		System.setProperty("webdriver.chrome.driver", ".\\lib\\chromedriver.exe");
-		driver = new ChromeDriver();
+		// System.setProperty("webdriver.chrome.driver", ".\\lib\\chromedriver.exe");
+		// driver = new ChromeDriver();
 
 		// Firefox
-		// driver = new FirefoxDriver();
-		driver.get("https://google.com");
+		driver = new FirefoxDriver();
+		driver.get("http://live.guru99.com");
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 	}
 
 	@Test
-	public void TC_01_CheckInvinonment() {
+	public void TC_01_Verify_URL_and_title() {
 		String homePageTittle = driver.getTitle();
-		Assert.assertEquals(homePageTittle, "Google");
+		Assert.assertEquals(homePageTittle, "Home page");
+		driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+		driver.findElement(By.xpath("//*[@title='Create an Account']")).click();
+		driver.navigate().back();
+		Assert.assertEquals(driver.getCurrentUrl(), "http://live.guru99.com/index.php/customer/account/login/");
+		driver.navigate().forward();
+		Assert.assertEquals(driver.getCurrentUrl(), "http://live.guru99.com/index.php/customer/account/create/");
 	}
 
 	@AfterClass
 	public void afterClass() {
+		driver.quit();
 	}
 
 }
